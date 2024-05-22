6381f52c491a2a4426577a93bde5821195d31681
Vedbhanushali <19ce005@charusat.edu.in>
2024-05-22 10:46:16 +0530

    Setup project with front on remix.js on ts, tailwindcss.

---
+++ front/app/routes/_index.tsx
@@ -0,0 +1,12 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return <h1 className="text-10xl font-bold underline bg-green-600">Login</h1>;
+}
