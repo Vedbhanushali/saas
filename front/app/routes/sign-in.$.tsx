240dc461d79b329459c5fadc3f71c58e470c23df
Vedbhanushali <19ce005@charusat.edu.in>
2025-01-19 23:48:06 +0530

    Refactor sign-in and sign-up pages for improved layout and styling

--- front/app/routes/sign-in.$.tsx
+++ front/app/routes/sign-in.$.tsx
@@ -2,8 +2,7 @@ import { SignIn } from "@clerk/remix";
 
 export default function SignInPage() {
   return (
-    <div>
-      <h1>Sign In route</h1>
+    <div className="flex items-center justify-center h-screen">
       <SignIn />
     </div>
   );
